8fa487385c247c6496ba820b4cfac3beaaa4be99
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-20 10:57:03 -0400

    chore: chases rename

--- init.zsh
+++ init.zsh
@@ -7,7 +7,7 @@
 ######################################################################
 p6df::modules::R::deps() {
   ModuleDeps=(
-    p6m7g8/p6common
+    p6m7g8-dotfiles/p6common
     viking/Renv
     viking/R-build
   )
