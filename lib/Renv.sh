cf5dc819c1ba262a9ea535321bc917167bb7f74c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-04-28 11:20:09 -0400

    chore: chases p6common refactor

--- lib/Renv.sh
+++ lib/Renv.sh
@@ -8,7 +8,7 @@
 ######################################################################
 p6df::modules::R::Renv::latest::installed() {
 
-  Renv install -l | p6_filter_row_select '"[0-9]"' | p6_filter_row_from_end "2" | p6_filter_spaces_strip
+  Renv install -l | p6_filter_row_select "[0-9]" | p6_filter_row_from_end "2" | p6_filter_spaces_strip
 }
 
 ######################################################################
@@ -20,5 +20,5 @@ p6df::modules::R::Renv::latest::installed() {
 ######################################################################
 p6df::modules::R::Renv::latest() {
 
-  Renv install -l | p6_filter_row_select '"[0-9]"' | p6_filter_row_last "1" | p6_filter_spaces_strip
+  Renv install -l | p6_filter_row_select "[0-9]" | p6_filter_row_last "1" | p6_filter_spaces_strip
 }
